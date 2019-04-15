7c8f82707b83999b7498ecb3d3c82160f843ce1f
Shirley Chu <shirley.cchu@gmail.com>
2019-04-15 16:51:36 -0700

    passed some passenger model tests, added routes, added homepages controller

--- test/models/passenger_test.rb
+++ test/models/passenger_test.rb
@@ -1,8 +1,9 @@
 require "test_helper"
+require "pry"
 
 describe Passenger do
   let (:new_passenger) {
-    Passenger.new(name: "Kari", phone_number: "111-111-1211")
+    Passenger.new(name: "Kari", phone_num: "111-111-1211")
   }
   it "can be instantiated" do
     # Assert
@@ -12,7 +13,7 @@ describe Passenger do
   it "will have the required fields" do
     # Arrange
     passenger = Passenger.first
-    [:name, :phone_number].each do |field|
+    [:name, :phone_num].each do |field|
 
       # Assert
       expect(passenger).must_respond_to field
@@ -23,6 +24,10 @@ describe Passenger do
     it "can have many trips" do
       # Arrange
       passenger = Passenger.first
+
+      passenger.trips.create!
+
+      # binding.pry
 
       # Assert
       expect(passenger.trips.count).must_be :>, 0
@@ -45,7 +50,7 @@ describe Passenger do
 
     it "must have a phone number" do
       # Arrange
-      new_passenger.phone_number = nil
+      new_passenger.phone_num = nil
 
       # Assert
       expect(new_passenger.valid?).must_equal false
