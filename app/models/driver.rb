9d85f5d7694c53e88298407167b2fb6e000536bb
kateannnichols <kateannnichols@gmail.com>
2019-04-17 18:33:05 -0700

    drivers

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,16 @@
 class Driver < ApplicationRecord
   has_many :trips
+  validates :name, presence: true
+  validates :vin, presence: true
+
+  def total_earnings
+  #The driver gets 80% of the trip cost after a fee of $1.65 is subtracted
+    self.trips.sum { |trip| trip.cost ? trip.cost : 0 }
+  end
+
+  def avg_rating
+    #add formula to return average rating 
+    # self.trips.rating { |trip| trip.rating ? trip.rating : 0 }
+  end
 end
+
