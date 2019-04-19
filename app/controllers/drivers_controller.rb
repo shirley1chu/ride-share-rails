7b9010ffa90d59597745c3928dd8fefe1e36c4e5
kateannnichols <kateannnichols@gmail.com>
2019-04-19 09:30:56 -0700

    fixed drivers_controller.rb

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -70,5 +70,7 @@ class DriversController < ApplicationController
     
       def driver_params
         return params.require(:driver).permit(:name, :vin)
-      end    
-end
+      end   
+    end
+end 
+
